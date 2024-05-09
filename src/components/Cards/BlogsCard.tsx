0771f3047eb448d96416008c7ddb1ceb20a6deef
Tipchan Songtheng <tipchans777@gmail.com>
2024-05-09 20:45:35 +0530

    create blogs section

--- src/components/Cards/BlogsCard.tsx
+++ src/components/Cards/BlogsCard.tsx
@@ -1,5 +1,43 @@
-const BlogsCard = () => {
-  return <div>BlogsCard</div>;
+import { blogsCardProps } from "../../types/types";
+
+const BlogsCard = (props: blogsCardProps) => {
+  return (
+    <div
+      key={props.id}
+      className="w-80 flex flex-col justify-between gap-3 hover:-translate-y-2 hover:scale-105 hover:drop-shadow-lg transition duration-400 ease-in-out  cursor-pointer bg-white rounded-lg group"
+    >
+      <figure className="relative w-full h-[215px]">
+        <img src={props.blogImage} alt="props Image" className="rounded-t-lg" />
+      </figure>
+
+      <div className="p-3 flex flex-col gap-1">
+        <div className="flex items-center gap-2">
+          <p className="text-red-700">{props.topic}</p>
+          <p className="text-sm text-gray-500">{`· ${props.time} read`}</p>
+        </div>
+
+        <h3 className="text-lg font-semibold text-gray-700">{props.heading}</h3>
+        <p className="text-sm text-justify text-gray-500 tracking-tight">
+          {props.description}
+        </p>
+      </div>
+
+      <div className="p-3 flex gap-2">
+        <figure className="w-8 h-8 rounded-full">
+          <img
+            src={props.authorImage}
+            alt="author-image"
+            className="rounded-full"
+          />
+        </figure>
+
+        <div>
+          <p className="font-semibold text-xs">{props.authorName}</p>
+          <p className="text-sm text-gray-500">Date</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BlogsCard;
--- src/components/Main/Blogs/Blogs.tsx
+++ src/components/Main/Blogs/Blogs.tsx
@@ -1,5 +1,92 @@
+import BlogsCard from "../../Cards/BlogsCard";
+import cardImg from "../../../assets/images/blogs_card_image.jpg";
+import authorImg from "../../../assets/images/author_image.jpg";
+import ViewAllButton from "../../Buttons/ViewAllButton";
+import { Link } from "react-router-dom";
+
+const cardsData = [
+  {
+    id: 1,
+    topic: "Sociology",
+    time: "3 min",
+    blogImage: cardImg,
+    heading: "Social Movements and Activism",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    authorImage: authorImg,
+    authorName: "Emily Chang",
+  },
+  {
+    id: 2,
+    topic: "technology",
+    time: "5 min",
+    blogImage: cardImg,
+    heading: "What's New in 2024 Tech",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    authorImage: authorImg,
+    authorName: "Benjamin Patel",
+  },
+  {
+    id: 3,
+    topic: "History",
+    time: "8 min",
+    blogImage: cardImg,
+    heading: "The Industrial Revolution",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    authorImage: authorImg,
+    authorName: "Jessica Rodriguez",
+  },
+];
+
+// async function getData() {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch data");
+//   }
+
+//   return res.json();
+// }
+
 const Blogs = () => {
-  return <div>Blogs</div>;
+  // const data: blogsCardProps = await getData();
+
+  const cardDataHome = cardsData.slice(0, 3);
+
+  return (
+    <section className="w-full 2xl:px-52 lg:py-24 xl:px-24 lg:px-16 py-16 px-8 flex flex-col gap-16 bg-gray-200">
+      <h2 className="lg:text-4xl text-3xl font-bold lg:mb-6 mb-0 text-gray-700">
+        Our latest Blogs
+      </h2>
+      <div className="w-full flex justify-evenly gap-8 items-center flex-wrap">
+        {cardDataHome.map((data) => (
+          <BlogsCard key={data.id} {...data} />
+        ))}
+      </div>
+
+      <div className="flex justify-center">
+        <Link to="/blogs">
+          <ViewAllButton>
+            View All
+            <svg
+              className="size-8"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 50 50"
+              id="arrow"
+            >
+              <path
+                className="transition fill-red-800 group-hover:fill-white"
+                fill="none"
+                d="M1 26h43.586l-6.293 6.293 1.414 1.414L48.414 25l-8.707-8.707-1.414 1.414L44.586 24H1z"
+              />
+            </svg>
+          </ViewAllButton>
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default Blogs;
