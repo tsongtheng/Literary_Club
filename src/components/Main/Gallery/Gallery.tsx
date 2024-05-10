0ec71c9a4e0dac722b134a19b58e8383240b7cc0
Tipchan Songtheng <tipchans777@gmail.com>
2024-05-10 12:59:16 +0530

    update gallery section hover effect

--- src/components/Main/Gallery/Gallery.tsx
+++ src/components/Main/Gallery/Gallery.tsx
@@ -6,131 +6,145 @@ const Gallery = () => {
     <section
       className={`${classes.gallery_container} xl:gap-4 sm:gap-3 gap-2  xl:p-4 sm:p-3 p-2`}
     >
-      <figure className={`${classes.gallery__img_1} relative cursor-pointer`}>
+      <figure
+        className={`${classes.gallery__img_1} relative cursor-pointer group`}
+      >
         <img
           className="object-cover w-full h-full block"
           src={galleryImg}
           alt="gallery-image-1"
         />
-        <div className="w-full h-full top-0 left-0 absolute bg-black bg-opacity-30 opacity-0 hover:opacity-100 transition duration-200"></div>
+        <div className="w-full h-full top-0 left-0 absolute bg-black bg-opacity-30 opacity-0 group-hover:opacity-100 scale-50 group-hover:scale-100 transition duration-300"></div>
       </figure>
-      <figure className={`${classes.gallery__img_2} relative cursor-pointer`}>
+      <figure
+        className={`${classes.gallery__img_2} relative cursor-pointer group`}
+      >
         <img
           className="object-cover w-full h-full block"
           src={galleryImg}
           alt="gallery-image-2"
         />
-        <div className="w-full h-full top-0 left-0 absolute bg-black bg-opacity-30 opacity-0 hover:opacity-100 transition duration-200"></div>
+        <div className="w-full h-full top-0 left-0 absolute bg-black bg-opacity-30 opacity-0 group-hover:opacity-100 scale-50 group-hover:scale-100 transition duration-300"></div>
       </figure>
-      <figure className={`${classes.gallery__img_3} relative cursor-pointer`}>
+      <figure
+        className={`${classes.gallery__img_3} relative cursor-pointer group`}
+      >
         <img
-          className="object-cover w-full h-full w-full h-full block"
+          className="object-cover w-full h-full block"
           src={galleryImg}
           alt="gallery-image-3"
         />
-        <div className="w-full h-full top-0 left-0 absolute bg-black bg-opacity-30 opacity-0 hover:opacity-100 transition duration-200"></div>
+        <div className="w-full h-full top-0 left-0 absolute bg-black bg-opacity-30 opacity-0 group-hover:opacity-100 scale-50 group-hover:scale-100 transition duration-300"></div>
       </figure>
-      <figure className={`${classes.gallery__img_4} relative cursor-pointer`}>
+      <figure
+        className={`${classes.gallery__img_4} relative cursor-pointer group`}
+      >
         <img
           className="object-cover w-full h-full block"
           src={galleryImg}
           alt="gallery-image-4"
         />
-        <div className="w-full h-full top-0 left-0 absolute bg-black bg-opacity-30 opacity-0 hover:opacity-100 transition duration-200"></div>
+        <div className="w-full h-full top-0 left-0 absolute bg-black bg-opacity-30 opacity-0 group-hover:opacity-100 scale-50 group-hover:scale-100 transition duration-300"></div>
       </figure>
-      <figure className={`${classes.gallery__img_5} relative cursor-pointer`}>
+      <figure
+        className={`${classes.gallery__img_5} relative cursor-pointer group`}
+      >
         <img
           className="object-cover w-full h-full block"
           src={galleryImg}
           alt="gallery-image-5"
         />
-        <div className="w-full h-full top-0 left-0 absolute bg-black bg-opacity-30 opacity-0 hover:opacity-100 transition duration-200"></div>
+        <div className="w-full h-full top-0 left-0 absolute bg-black bg-opacity-30 opacity-0 group-hover:opacity-100 scale-50 group-hover:scale-100 transition duration-300"></div>
       </figure>
-      <figure className={`${classes.gallery__img_6} relative cursor-pointer`}>
+      <figure
+        className={`${classes.gallery__img_6} relative cursor-pointer group`}
+      >
         <img
           className="object-cover w-full h-full block"
           src={galleryImg}
           alt="gallery-image-6"
         />
-        <div className="w-full h-full top-0 left-0 absolute bg-black bg-opacity-30 opacity-0 hover:opacity-100 transition duration-200"></div>
+        <div className="w-full h-full top-0 left-0 absolute bg-black bg-opacity-30 opacity-0 group-hover:opacity-100 scale-50 group-hover:scale-100 transition duration-300"></div>
       </figure>
-      <figure className={`${classes.gallery__img_7} relative cursor-pointer`}>
+      <figure
+        className={`${classes.gallery__img_7} relative cursor-pointer group`}
+      >
         <img
           className="object-cover w-full h-full block"
           src={galleryImg}
           alt="gallery-image-7"
         />
-        <div className="w-full h-full top-0 left-0 absolute bg-black bg-opacity-30 opacity-0 hover:opacity-100 transition duration-200"></div>
+        <div className="w-full h-full top-0 left-0 absolute bg-black bg-opacity-30 opacity-0 group-hover:opacity-100 scale-50 group-hover:scale-100 transition duration-300"></div>
       </figure>
       <figure
-        className={`${classes.gallery__img_8} relative cursor-pointer lg:block hidden`}
+        className={`${classes.gallery__img_8} relative cursor-pointer group lg:block hidden`}
       >
         <img
           className="object-cover w-full h-full block"
           src={galleryImg}
           alt="gallery-image-8"
         />
-        <div className="w-full h-full top-0 left-0 absolute bg-black bg-opacity-30 opacity-0 hover:opacity-100 transition duration-200"></div>
+        <div className="w-full h-full top-0 left-0 absolute bg-black bg-opacity-30 opacity-0 group-hover:opacity-100 scale-50 group-hover:scale-100 transition duration-300"></div>
       </figure>
       <figure
-        className={`${classes.gallery__img_9} relative cursor-pointer lg:block hidden`}
+        className={`${classes.gallery__img_9} relative cursor-pointer group lg:block hidden`}
       >
         <img
           className="object-cover w-full h-full block"
           src={galleryImg}
           alt="gallery-image-9"
         />
-        <div className="w-full h-full top-0 left-0 absolute bg-black bg-opacity-30 opacity-0 hover:opacity-100 transition duration-200"></div>
+        <div className="w-full h-full top-0 left-0 absolute bg-black bg-opacity-30 opacity-0 group-hover:opacity-100 scale-50 group-hover:scale-100 transition duration-300"></div>
       </figure>
       <figure
-        className={`${classes.gallery__img_10} relative cursor-pointer lg:block hidden`}
+        className={`${classes.gallery__img_10} relative cursor-pointer group lg:block hidden`}
       >
         <img
           className="object-cover w-full h-full block"
           src={galleryImg}
           alt="gallery-image-10"
         />
-        <div className="w-full h-full top-0 left-0 absolute bg-black bg-opacity-30 opacity-0 hover:opacity-100 transition duration-200"></div>
+        <div className="w-full h-full top-0 left-0 absolute bg-black bg-opacity-30 opacity-0 group-hover:opacity-100 scale-50 group-hover:scale-100 transition duration-300"></div>
       </figure>
       <figure
-        className={`${classes.gallery__img_11} relative cursor-pointer lg:block hidden`}
+        className={`${classes.gallery__img_11} relative cursor-pointer group lg:block hidden`}
       >
         <img
           className="object-cover w-full h-full block"
           src={galleryImg}
           alt="gallery-image-11"
         />
-        <div className="w-full h-full top-0 left-0 absolute bg-black bg-opacity-30 opacity-0 hover:opacity-100 transition duration-200"></div>
+        <div className="w-full h-full top-0 left-0 absolute bg-black bg-opacity-30 opacity-0 group-hover:opacity-100 scale-50 group-hover:scale-100 transition duration-300"></div>
       </figure>
       <figure
-        className={`${classes.gallery__img_12} relative cursor-pointer lg:block hidden`}
+        className={`${classes.gallery__img_12} relative cursor-pointer group lg:block hidden`}
       >
         <img
           className="object-cover w-full h-full block"
           src={galleryImg}
           alt="gallery-image-12"
         />
-        <div className="w-full h-full top-0 left-0 absolute bg-black bg-opacity-30 opacity-0 hover:opacity-100 transition duration-200"></div>
+        <div className="w-full h-full top-0 left-0 absolute bg-black bg-opacity-30 opacity-0 group-hover:opacity-100 scale-50 group-hover:scale-100 transition duration-300"></div>
       </figure>
       <figure
-        className={`${classes.gallery__img_13} relative cursor-pointer lg:block hidden`}
+        className={`${classes.gallery__img_13} relative cursor-pointer group lg:block hidden`}
       >
         <img
           className="object-cover w-full h-full block"
           src={galleryImg}
           alt="gallery-image-13"
         />
-        <div className="w-full h-full top-0 left-0 absolute bg-black bg-opacity-30 opacity-0 hover:opacity-100 transition duration-200"></div>
+        <div className="w-full h-full top-0 left-0 absolute bg-black bg-opacity-30 opacity-0 group-hover:opacity-100 scale-50 group-hover:scale-100 transition duration-300"></div>
       </figure>
       <figure
-        className={`${classes.gallery__img_14} relative cursor-pointer lg:block hidden`}
+        className={`${classes.gallery__img_14} relative cursor-pointer group lg:block hidden`}
       >
         <img
           className="object-cover w-full h-full block"
           src={galleryImg}
           alt="gallery-image-14"
         />
-        <div className="w-full h-full top-0 left-0 absolute bg-black bg-opacity-20 opacity-0 hover:opacity-100 transition duration-200"></div>
+        <div className="w-full h-full top-0 left-0 absolute bg-black bg-opacity-20 opacity-0 group-hover:opacity-100 scale-50 group-hover:scale-100 transition duration-300"></div>
       </figure>
     </section>
   );
