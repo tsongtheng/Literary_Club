99a9ac7c3be45d0473b3218e765b8d962f5a0b7e
Tipchan Songtheng <tipchans777@gmail.com>
2024-05-10 12:08:07 +0530

    create FAQ section

--- src/components/Main/FAQ/FAQComponent.tsx
+++ src/components/Main/FAQ/FAQComponent.tsx
@@ -1,5 +1,58 @@
+import FAQItems from "./FAQItems";
+
+type faqDataObj = {
+  id: number;
+  question: string;
+  answer: string;
+};
+
+const faqData: faqDataObj[] = [
+  {
+    id: 1,
+    question: "What is Tailwind CSS?",
+    answer:
+      "Tailwind CSS is a utility-first CSS framework for creating custom designs without having to leave your HTML.",
+  },
+  {
+    id: 2,
+    question: "What is Next.js?",
+    answer:
+      "Next.js is a React framework that enables functionality such as server-side rendering, static site generation, and more.",
+  },
+  {
+    id: 3,
+    question: "Lorem ipsum dolor sit amet?",
+    answer:
+      "Fusce feugiat nunc vel purus auctor, eget tincidunt risus pulvinar. Vivamus at suscipit tortor, a sagittis quam. Morbi nec gravida augue, sed egestas lorem. Ut eget quam id arcu bibendum rhoncus non a libero.",
+  },
+  {
+    id: 4,
+    question: "Nunc laoreet faucibus ligula?",
+    answer:
+      "Next.js is a React framework that enables functionality such as server-side rendering, static site generation, and more.",
+  },
+  {
+    id: 5,
+    question: "Etiam turpis tellus, ullamcorper a pretium?",
+    answer:
+      " Etiam turpis tellus, ullamcorper a pretium a, imperdiet in arcu. Donec sodales enim vel ligula pellentesque molestie. Quisque euismod condimentum auctor. Morbi arcu enim. Donec sodales enim vel",
+  },
+];
+
 const FAQComponent = () => {
-  return <div>FAQComponent</div>;
+  return (
+    <section
+      className="w-full 2xl:px-52 lg:py-32 py-16 px-8 flex flex-col items-center gap-20"
+      id="faq"
+    >
+      <h2 className="lg:text-4xl text-3xl text-center font-bold lg:mb-6 mb-0 text-gray-700">
+        Frequently asked questions{" "}
+      </h2>
+      <div className="xl:w-4/6 lg:w-3/4 md:w-5/6 sm:w-11/12 w-full flex flex-col gap-4">
+        <FAQItems faqData={faqData} />
+      </div>
+    </section>
+  );
 };
 
 export default FAQComponent;
--- src/components/Main/FAQ/FAQItems.tsx
+++ src/components/Main/FAQ/FAQItems.tsx
@@ -1,5 +1,58 @@
-const FAQItems = () => {
-  return <div>FAQItems</div>;
+import React, { useState } from "react";
+
+interface FAQItemsObj {
+  id: number;
+  question: string;
+  answer: string;
+}
+
+interface FAQProps {
+  faqData: FAQItemsObj[];
+}
+
+const FAQItems: React.FC<FAQProps> = ({ faqData }) => {
+  const [activeId, setActiveId] = useState<number | null>(null);
+
+  const toggleActiveId = (id: number) => {
+    setActiveId(activeId === id ? null : id);
+  };
+  return (
+    <>
+      {faqData.map((faq) => (
+        <div
+          key={faq.id}
+          className="w-full border-[1px] rounded-xl p-5 border-gray-300 hover:border-gray-500 cursor-pointer"
+          onClick={() => toggleActiveId(faq.id)}
+        >
+          <div className="flex gap-3 justify-between items-center">
+            <h3 className="text-lg font-semibold text-gray-700">
+              {faq.question}
+            </h3>
+            <svg
+              className={`w-4 h-4 text-gray-700 dark:text-gray-700 ${
+                activeId === faq.id ? "rotate-180" : ""
+              }`}
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 8"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m1 1 5.326 5.7a.909.909 0 0 0 1.348 0L13 1"
+              />
+            </svg>
+          </div>
+          {activeId === faq.id && (
+            <p className="text-justify text-gray-500 pt-5">{faq.answer}</p>
+          )}
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default FAQItems;
