6e101062adf4483552952ec90c3fcc8bef522be8
Tipchan Songtheng <tipchans777@gmail.com>
2024-05-10 12:05:03 +0530

    create Events section

--- src/components/Main/Events/Events.tsx
+++ src/components/Main/Events/Events.tsx
@@ -1,5 +1,52 @@
-const Events = () => {
-  return <div>Events</div>;
+import { Link } from "react-router-dom";
+
+import EventsItem from "./EventsItems";
+import ViewAllButton from "../../Buttons/ViewAllButton";
+
+const eventsData = [
+  {
+    id: 1,
+    date: "Mon, Feb 04",
+    eventName: "Writing Workshops",
+    dateTime: "Feb, 2024, 10:00 AM - 3:00 PM",
+    location: "Wagun Ponthai, Community Hall",
+    details:
+      "Fusce feugiat nunc vel purus auctor, eget tincidunt risus pulvinar. Vivamus at suscipit tortor, a sagittis quam. Morbi nec gravida augue, sed egestas lorem. Ut eget quam id arcu bibendum rhoncus non a libero.",
+  },
+  {
+    id: 2,
+    date: "Sun, May 20",
+    eventName: "Book Discussions",
+    dateTime: "May 20, 2024, 1:00 PM - 5:00 PM",
+    location: "Google Meet",
+    details:
+      "Fusce feugiat nunc vel purus auctor, eget tincidunt risus pulvinar. Vivamus at suscipit tortor, a sagittis quam. Morbi nec gravida augue, sed egestas lorem. Ut eget quam id arcu bibendum rhoncus non a libero.Fusce feugiat nunc vel purus auctor, eget tincidunt risus pulvinar.",
+  },
+  {
+    id: 3,
+    date: "Wed, Aug 20",
+    eventName: "Poetry Slams",
+    dateTime: "Aug 20, 2024, 11:00 AM - 5:00 PM",
+    location: "Government Secondary School, Wagun Ponthai",
+    details:
+      "Fusce feugiat nunc vel purus auctor, eget tincidunt risus pulvinar. Vivamus at suscipit tortor, a sagittis quam. Morbi nec gravida augue, sed egestas lorem. Ut eget quam id arcu bibendum rhoncus non a libero.",
+  },
+];
+
+const Events: React.FC = () => {
+  return (
+    <section className="w-full 2xl:px-52 lg:py-32 py-16 px-8 flex flex-col items-center gap-12 bg-gray-200">
+      <h2 className="lg:text-4xl text-3xl text-center font-bold lg:mb-6 mb-0 text-gray-700">
+        Upcoming Events
+      </h2>
+      <div className="xl:w-4/6 lg:w-3/4 md:w-5/6 sm:w-11/12 w-full flex flex-col">
+        <EventsItem eventsData={eventsData} />
+      </div>
+      <Link to="/events">
+        <ViewAllButton>See All Events</ViewAllButton>
+      </Link>
+    </section>
+  );
 };
 
 export default Events;
--- src/components/Main/Events/EventsItems.tsx
+++ src/components/Main/Events/EventsItems.tsx
@@ -1,5 +1,84 @@
-const EventsItems = () => {
-  return <div>EventsItems</div>;
+"use client";
+
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import LearnMoreButton from "../../Buttons/LearnMoreButton";
+
+interface eventsItem {
+  id: number;
+  date: string;
+  eventName: string;
+  dateTime: string;
+  location: string;
+  details: string;
+}
+
+interface eventsProps {
+  eventsData: eventsItem[];
+}
+
+const EventsItems: React.FC<eventsProps> = ({ eventsData }) => {
+  const [activeId, setActiveId] = useState<number | null>(null);
+
+  const toggleActiveId = (id: number) => {
+    setActiveId(activeId === id ? null : id);
+  };
+
+  return (
+    <>
+      {eventsData.map((event) => (
+        <div
+          key={event.id}
+          className="w-full border-b-[1px] py-14 border-gray-400 hover:border-red-700 cursor-pointer"
+        >
+          <div className="flex justify-between items-center gap-4">
+            <div
+              className="capitalize"
+              onClick={() => toggleActiveId(event.id)}
+            >
+              <h3 className="font-bold text-gray-700">{event.date}</h3>
+              <div className="flex gap-2 items-center text-gray-700">
+                <p className="text-lg">{event.eventName}</p>
+                <svg
+                  className={`w-4 h-4 text-gray-700 dark:text-gray-700 ${
+                    activeId === event.id ? "rotate-180" : ""
+                  }`}
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 8"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 1 5.326 5.7a.909.909 0 0 0 1.348 0L13 1"
+                  />
+                </svg>
+              </div>
+            </div>
+            <Link to="/events">
+              <button className="text-white px-6 py-2 bg-red-800 xl:hover:bg-red-700 ">
+                Register
+              </button>
+            </Link>
+          </div>
+          {activeId === event.id && (
+            <div className="text-justify pt-5">
+              <p className="text-gray-700 capitalize">{event.dateTime}</p>
+              <p className="mb-4 text-gray-700 capitalize">{event.location}</p>
+              <p className="text-gray-500 font-light">{event.details}</p>
+
+              <Link to="/events">
+                <LearnMoreButton />
+              </Link>
+            </div>
+          )}
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default EventsItems;
